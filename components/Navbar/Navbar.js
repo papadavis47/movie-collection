65a6825f53d53743e2fdd649bf93801243b40dc5
John William Davis <jwdavisdeveloper@gmail.com>
2022-05-25 11:43:05 -0700

    Rename menu svg, make navbar sticky when scrolling

--- components/Navbar/Navbar.js
+++ components/Navbar/Navbar.js
@@ -4,7 +4,7 @@ import { ImStackoverflow, ImGithub, ImTwitter } from 'react-icons/im'
 
 function Navbar() {
   return (
-    <nav className="mb-10 text-white bg-blue-500/80">
+    <nav className="sticky top-0 mb-10 text-white bg-blue-500/80">
       {/* container div below */}
       <div className="px-4 mx-auto max-w-7xl">
         {/* sub-container */}
@@ -14,7 +14,7 @@ function Navbar() {
             <div>
               <Link href="/">
                 <a className="flex items-center px-2 py-4">
-                  <Image src="/white-surfboard.svg" width={60} height={60} />
+                  <Image src="/surfboard.svg" width={60} height={60} />
                   <span className="ml-4 text-3xl tracking-tighter text-white uppercase">
                     jwd
                   </span>
