c7baf15f4428b75fc9e2c5c9e9dac62a46a73651
John William Davis <jwdavisdeveloper@gmail.com>
2022-05-25 11:38:59 -0700

    Create Navbar with social links and menu for larger screens

--- components/Layout/Layout.js
+++ components/Layout/Layout.js
@@ -1,14 +1,15 @@
 import Head from 'next/head'
 import Footer from '../Footer'
+import Navbar from '../Navbar'
 
 function Layout({ children }) {
   return (
-    <div className="flex flex-col min-h-screen py-2">
+    <div className="flex flex-col min-h-screen">
       <Head>
         <title>My Movie Collection</title>
         <link rel="icon" href="/surfing.ico" />
       </Head>
-
+      <Navbar />
       <main className="flex-1 w-full px-10">{children}</main>
       <Footer />
     </div>
---
+++ components/Navbar/Navbar.js
@@ -0,0 +1,56 @@
+import Link from 'next/link'
+import Image from 'next/image'
+import { ImStackoverflow, ImGithub, ImTwitter } from 'react-icons/im'
+
+function Navbar() {
+  return (
+    <nav className="mb-10 text-white bg-blue-500/80">
+      {/* container div below */}
+      <div className="px-4 mx-auto max-w-7xl">
+        {/* sub-container */}
+        <div className="flex justify-between">
+          {/* left side of nav */}
+          <div>
+            <div>
+              <Link href="/">
+                <a className="flex items-center px-2 py-4">
+                  <Image src="/white-surfboard.svg" width={60} height={60} />
+                  <span className="ml-4 text-3xl tracking-tighter text-white uppercase">
+                    jwd
+                  </span>
+                </a>
+              </Link>
+            </div>
+          </div>
+          {/* right side of nav */}
+          <div className="items-center hidden pr-4 mr-4 space-x-4 justify-self-center md:flex">
+            <Link href="#">
+              <a className="px-2 py-6 text-xl font-bold text-white">About</a>
+            </Link>
+            <Link href="#">
+              <a className="px-2 py-6 text-xl font-bold">Tech Stack</a>
+            </Link>
+            <Link href="#">
+              <a className="px-2 py-6 text-xl font-bold text-white">
+                View Collection
+              </a>
+            </Link>
+          </div>
+          <div className="flex items-center space-x-5">
+            <a href="#">
+              <ImGithub size={30} />
+            </a>
+            <a href="#">
+              <ImTwitter size={30} />
+            </a>
+            <a href="#">
+              <ImStackoverflow size={30} />
+            </a>
+          </div>
+        </div>
+      </div>
+    </nav>
+  )
+}
+
+export default Navbar
